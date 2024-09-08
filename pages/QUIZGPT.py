8a599646d54f245b4ec372cf1c25495cf1a8a9d3
jyooDev <gina07099@gmail.com>
2024-09-08 09:22:55 -0500

    feat: guiz-gpt created

---
+++ pages/QUIZGPT.py
@@ -0,0 +1,96 @@
+import streamlit as st
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from rag.retriever import load_split_docs, get_docs_from_wikipedia_retriever
+from rag.model import get_openai_model
+from rag.prompt.quiz_gpt import get_question_prompt
+from rag.chat import format_docs
+from rag.validation import validate_api_key
+from rag.chain import run_quiz_chain
+from rag.functions.quiz_function import function
+st.set_page_config(
+    page_title = "QuizGPT",
+    page_icon = "",
+    layout = "centered",
+)
+
+if "valid_api_key" not in st.session_state:
+    st.session_state["valid_api_key"] = False
+st.title("QuizGPT")
+
+
+with st.sidebar:
+    docs = None
+    topic = None
+    api_key = st.text_input("Enter your API key.", type="password")
+    key_validation_button = st.button("Validate")
+    if key_validation_button:
+        st.session_state["valid_api_key"] = validate_api_key(api_key)
+    if st.session_state["valid_api_key"]:
+        choice = st.selectbox("Choose what you want to use.", (
+            "File",
+            "Wikipedia Article",
+        ))
+        if choice == "File":
+            file = st.file_uploader("Upload a .txt, .pdf, .docs, .md files only", type=[
+                                "pdf", "txt", "docx", "md"])
+            if file:
+                docs = load_split_docs(
+                file = file,
+                service = "quiz",
+                )
+        else:
+            topic = st.text_input("Search Wikipedia..").lower()
+            if topic:
+                docs = get_docs_from_wikipedia_retriever(topic)
+        difficulty = st.radio(
+            "Select the Quiz Difficulty",
+            ["Easy", "Intermediate", "Hard"])
+
+if not docs or not api_key:
+    st.markdown(
+        """
+    Welcome to QuizGPT!
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge.
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+    """
+    )    
+else:     
+    llm = get_openai_model(api_key).bind(
+        function_call={
+            "name": "generate_quiz",
+        },
+        functions=[
+            function,
+        ]
+    )
+    question_prompt = get_question_prompt()
+    chain = question_prompt | llm
+    response = run_quiz_chain(
+        _docs=docs,
+        topic=topic if topic else file.name,
+        difficulty = difficulty,
+        _chain = chain,)
+    st.write(response)
+    with st.form("questions_form"):
+        answer_ct = 0
+        for question in response:
+            st.write(question["question"])
+            value = st.radio(
+                "Select an option.",
+                [answer["answer"] for answer in question["answers"]],
+                index=None,
+            )
+            if {"answer": value, "correct": True} in question["answers"]:
+                st.success("Correct!")
+                answer_ct += 1
+            elif value is not None:
+                st.error("Wrong!")
+
+        button = st.form_submit_button()   
+        if button and answer_ct == 10:
+            st.balloons()
+            st.header("Congratulation!")
+            answer_ct = 0
+        elif button and answer_ct != 10:
+            st.header(f"Score: {answer_ct}/10")
+            answer_ct = 0
--- pages/documentgpt.py
+++ pages/documentgpt.py
@@ -1,85 +1,17 @@
 import os
 import openai
 from langchain.prompts import ChatPromptTemplate
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
-from langchain.storage import LocalFileStore
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
+from rag.retriever import embed_file
+from rag.chat import save_message, send_message
+from rag.model import get_openai_model
 
 st.set_page_config(
     page_title="Document GPT",
     page_icon="📄",
     layout="wide",
 )
-
-
-class ChatCallbackHandler(BaseCallbackHandler):
-    message = ""
-
-    def on_llm_start(self, *args, **kwargs):
-        self.message_box = st.empty()
-
-    def on_llm_end(self, *args, **kwargs):
-        save_message(self.message, "ai")
-
-    def on_llm_new_token(self, token, *args, **kwargs):
-        self.message += token
-        self.message_box.markdown(self.message)
-
-
-
-@st.cache_resource
-def get_openai_model(api_key):
-    return ChatOpenAI(
-        temperature=0.1,
-        streaming=True,
-        callbacks=[ChatCallbackHandler()],
-        openai_api_key=api_key,
-    )
-
-
-@st.cache_data(show_spinner="Embedding File...")
-def embed_file(file, api_key):
-    file_path = f"./.cache/files/{file.name}"  
-    cache_dir_path = f"./.cache/embeddings/{file.name}"    
-    os.makedirs(os.path.dirname(file_path), exist_ok=True) 
-    os.makedirs(os.path.dirname(cache_dir_path), exist_ok=True)  
-    cache_dir = LocalFileStore(cache_dir_path) 
-
-    file_content = file.read()
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=100,
-    )
-    loader = UnstructuredFileLoader(file_path)
-    docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
-        embeddings, cache_dir)
-    vectorstore = FAISS.from_documents(docs, cached_embeddings)
-    retriever = vectorstore.as_retriever()
-    return retriever
-
-
-def save_message(message, role):
-    st.session_state["messages"].append({"message": message, "role": role})
-
-
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.markdown(message)
-    if save:
-        save_message(message, role)
-
 def paint_history():
     for message in st.session_state["messages"]:
         send_message(message["message"], message["role"], save=False)
@@ -126,13 +58,12 @@ if api_key:
         os.environ["OPENAI_API_KEY"] = api_key
         st.success("Valid API Key")
         if file:
-            retriever = embed_file(file, api_key)
-            llm = ChatOpenAI(
-                temperature=0.1,
-                streaming=True,
-                callbacks=[ChatCallbackHandler()],
-                openai_api_key=api_key,
+            retriever = embed_file(
+                file = file, 
+                service = "document",
+                api_key= api_key
             )
+            llm = get_openai_model()
             send_message("I'm ready! Ask away!", "ai", save=False)
             paint_history()
             message = st.chat_input("Ask Anything! about your file...")
