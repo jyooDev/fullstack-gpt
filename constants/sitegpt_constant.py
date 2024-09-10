d9b82026fac25c91452ffa4675f6c40b200e1694
jyooDev <gina07099@gmail.com>
2024-09-10 13:14:18 -0500

    refactor: moved urls to constants folder

---
+++ constants/sitegpt_constant.py
@@ -0,0 +1,5 @@
+cloudflare_filter_urls = ['https://developers.cloudflare.com/ai-gateway/',
+               'https://developers.cloudflare.com/vectorize/',
+               'https://developers.cloudflare.com/workers-ai/',]
+
+cloudflare_sitemap_url = "https://developers.cloudflare.com/sitemap-0.xml"
